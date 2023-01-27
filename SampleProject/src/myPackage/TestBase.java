5193bff5fb50b2e0ddf0a22b3d6a84b003e5eb06
shweta <shweta07.baderia@gmail.com>
2023-01-27 16:53:23 +0000

    HomePage work done
    hi
    i had done homepage work

--- SampleProject/src/myPackage/TestBase.java
+++ SampleProject/src/myPackage/TestBase.java
@@ -3,6 +3,6 @@ package myPackage;
 public class TestBase 
 {
 	public static void main (String []args) {
-System.out.println("basic task completed in project");
+System.out.println("basic task completed in ");
 }
 }
